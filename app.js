95f8b317f2812b2001527d13d113f7c4b18bbb8d
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2022-02-03 16:03:08 +0500

    Completed frontend designs

---
+++ app.js
@@ -0,0 +1,46 @@
+let wrapper = document.querySelector('.links');
+let toggle = document.querySelector('.bur');
+
+
+window.onload=function(){
+    
+    toggle.onclick=function(){
+    toggle.classList.toggle('active'); 
+    wrapper.classList.toggle('open')
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
